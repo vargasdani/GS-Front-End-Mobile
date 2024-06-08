248b9c0a72887a7e6f46200470d5a8b53626ca75
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-07 22:05:22 -0300

    Subindo nova versao

--- src/Tabs/Barreiras.tsx
+++ src/Tabs/Barreiras.tsx
@@ -1,11 +1,42 @@
-import {VStack,Text} from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { VStack, Text, Spinner } from 'native-base';
 import { CardBarreira } from '../componentes/CardBarreira';
-export default function Barreiras (){
-    return(
-        <VStack p={5} w="100%" bg="gray.100">
-            <Text fontSize="2xl" mb={5}>Barreiras Disponíveis</Text>
-            <CardBarreira/>
-        </VStack>
+import axios from 'axios';
 
-    )
+export default function Barreiras() {
+  const [barreiras, setBarreiras] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:3333/barreiras');
+        setBarreiras(response.data.barreiras);
+      } catch (error) {
+        console.error("Erro ao buscar dados da API", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <VStack p={5} w="100%" bg="gray.100">
+      <Text fontSize="2xl" mb={5}>Barreiras Disponíveis</Text>
+      {barreiras.map(barreira => (
+        <CardBarreira
+          key={barreira.id}
+          localizacao={barreira.localizacao}
+          capacidadeMax={barreira.capacidadeMax}
+          dataCapacidade={barreira.DataCapacidade}
+        />
+      ))}
+    </VStack>
+  );
 }
--- src/componentes/CardBarreira.tsx
+++ src/componentes/CardBarreira.tsx
@@ -1,19 +1,19 @@
-import{Text, Avatar, VStack}from 'native-base'
-import{barreira_flutuante} from'../assets/barreira_flutuante.jpeg'
-import { Botao } from './Botao'
-export function CardBarreira(){
-    return(
-        <VStack w="100%" bg="white" p={5} borderRadius='lg'shadow={2}>
-          <VStack>
-            <Avatar size="lg"source={barreira_flutuante} mt={5}/>
-            <Text>Barreira Flutuante</Text>
-            <Text>Contagem lixo:120kg</Text>
-            <Text>Última vez analisada em:05/06/2024</Text>
-          </VStack>
-            <Botao mt={5}>
-                Fazer retirada de lixo
-            </Botao>
+import { Text, Avatar, VStack } from 'native-base';
+import { barreira_flutuante } from '../assets/barreira_flutuante.jpeg';
+import { Botao } from './Botao';
 
-        </VStack>
-    )
+export function CardBarreira({ localizacao, capacidadeMax, dataCapacidade }) {
+  return (
+    <VStack w="100%" bg="white" p={5} borderRadius="lg" shadow={2}>
+      <VStack>
+        <Avatar size="lg" source={barreira_flutuante} mt={5} />
+        <Text>Localização: {localizacao}</Text>
+        <Text>Capacidade Máxima: {capacidadeMax} kg</Text>
+        <Text>Última vez analisada em: {dataCapacidade}</Text>
+      </VStack>
+      <Botao mt={5}>
+        Fazer retirada de lixo
+      </Botao>
+    </VStack>
+  );
 }
